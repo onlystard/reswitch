1946ca9c0cab0410aa269c13b121f18c0a9ed3a1
Pham Thien Tan <onlystard@gmail.com>
2024-01-25 12:04:04 +0700

    feat: update new deps

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,47 +1,47 @@
-import { Middleware, Tuple, applyMiddleware, configureStore, createSerializableStateInvariantMiddleware } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist'
-import { rootReducer, RootState } from './global-reducers'
-import { IPersistConfig } from 'utils/common'
+import {Middleware, Tuple, applyMiddleware, configureStore, createSerializableStateInvariantMiddleware} from "@reduxjs/toolkit";
+import {persistStore, persistReducer} from "redux-persist";
+import {rootReducer, RootState} from "./global-reducers";
+import {IPersistConfig} from "utils/common";
 
-import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import sagaMiddlewareFactory from 'redux-saga'
-import rootSaga from './sagas'
+import sagaMiddlewareFactory from "redux-saga";
+import rootSaga from "./sagas";
+import {MMKVStorages} from "config/Stores";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
-const middleware: Middleware<{}, RootState>[] = []
+const middleware: Middleware<{}, RootState>[] = [];
 const enhancers = [] as any[];
 
 /* ------------- Persister Reducer ------------- */
 const persistConfig: IPersistConfig<RootState> = {
-    key: 'root',
-    version: 1,
-    storage: AsyncStorage,
-    // blacklist: ['i18n'],
-    whitelist: ['uidToken'],
-    stateReconciler: autoMergeLevel2,
-}
-const persistedReducer = persistReducer(persistConfig, rootReducer as any)
+	key: "root",
+	version: 1,
+	storage: MMKVStorages,
+	// blacklist: ['i18n'],
+	whitelist: ["uidToken"],
+	stateReconciler: autoMergeLevel2
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer as any);
 
 /* ------------- Assemble Middleware ------------- */
-enhancers.push(applyMiddleware(...middleware))
+enhancers.push(applyMiddleware(...middleware));
 
 /* ------------- Saga Middleware ------------- */
 const sagaMiddleware = sagaMiddlewareFactory();
 middleware.push(sagaMiddleware as unknown as Middleware);
 
 const serializableMiddleware = createSerializableStateInvariantMiddleware({
-    ignoredActions: ['persist/PERSIST'],
-    ignoredPaths: [],
-})
-
-const store = configureStore({
-    reducer: persistedReducer,
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware as Middleware),
-    middleware: () => new Tuple(serializableMiddleware).concat(sagaMiddleware as Middleware),
+	ignoredActions: ["persist/PERSIST"],
+	ignoredPaths: []
 });
 
-sagaMiddleware.run(rootSaga)
+const store = configureStore({
+	reducer: persistedReducer,
+	// middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware as Middleware),
+	middleware: () => new Tuple(serializableMiddleware).concat(sagaMiddleware as Middleware)
+});
 
-export const persistor = persistStore(store)
-export type AppDispatch = typeof store.dispatch
-export default store
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
+export type AppDispatch = typeof store.dispatch;
+export default store;
